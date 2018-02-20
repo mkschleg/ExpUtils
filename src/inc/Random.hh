3efe8fb6114477a7943f1690282d021020979977
Matt <mkschleg@umail.iu.edu>
2018-02-20 16:37:04 -0700

    Updated Random Functions

--- src/inc/Random.hh
+++ src/inc/Random.hh
@@ -20,29 +20,33 @@ namespace Random{
       seed = getpid();
     }
     getGeneratorInstance().seed(seed);
-
   }
-  
+
+  /// \brief Random integer in range [0,max)
   inline int range(int max){
     static std::uniform_int_distribution<int> dist{};
     return dist(getGeneratorInstance(), decltype(dist)::param_type{0,max-1});
   }
+
+  /// \brief Random integer in range [min,max]
+  inline int range(int min, int max){
+    static std::uniform_int_distribution<int> dist{};
+    return dist(getGeneratorInstance(), decltype(dist)::param_type{min,max});
+  }
   
+  /// \brief Random number from the uniform distribution
   inline double uniform(){
     static std::uniform_real_distribution<double> dist{0,1};
     return dist(getGeneratorInstance());
   }
-  
+
+  /// \brief Random number from the Normal Distribuion
   inline double normal(double mu, double sigma){
     static std::normal_distribution<double> dist{};
     using param_t = decltype(dist)::param_type;
     return dist(getGeneratorInstance(), param_t{mu, sigma});
   }
-  //
-  // 
-  // void seed(int iseed = 0);
-  // int range(int max);
-  // double uniform();
-  // double normal(double mu, double sigma);
+
+
 }
 }
